9501f64fe1332c5734e16ef39dab7c139af144ee
jano <janototh@gmail.com>
2009-11-05 23:47:49 +0100

    Localization is set according to user's browser settings, default localizatin is English. Add groups localization, all tests ok.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  @auto_localize = true
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '508416363d2bbc1e88e700958a8ce24f'
@@ -22,4 +24,23 @@ class ApplicationController < ActionController::Base
     end
     @current_user = session[:user_id].nil? ? AnonymousUser.new : User.find(session[:user_id])
   end
+
+  before_filter :set_locale
+  if(@auto_localize)
+     def set_locale
+      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+      if(extract_locale_from_accept_language_header == "sk" || extract_locale_from_accept_language_header == "en")
+          I18n.locale = extract_locale_from_accept_language_header
+      else
+        I18n.locale = :en
+      end
+      logger.debug "* Locale set to '#{I18n.locale}'"
+    end
+  private
+    def extract_locale_from_accept_language_header
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first unless request.env['HTTP_ACCEPT_LANGUAGE'].nil?
+    end
+  end
+
+
 end
